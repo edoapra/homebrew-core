6fa5a96721e59462a2ee0303d718b8d11cfa6dbe
edoapra <edoardo.apra@gmail.com>
2023-03-10 18:13:54 -0800

    updates for release 7.2.0

--- Formula/nwchem.rb
+++ Formula/nwchem.rb
@@ -1,11 +1,10 @@
 class Nwchem < Formula
   desc "High-performance computational chemistry tools"
   homepage "https://nwchemgit.github.io"
-  url "https://github.com/nwchemgit/nwchem/releases/download/v7.0.2-release/nwchem-7.0.2-release.revision-b9985dfa-src.2020-10-12.tar.bz2"
-  version "7.0.2"
-  sha256 "d9d19d87e70abf43d61b2d34e60c293371af60d14df4a6333bf40ea63f6dc8ce"
+  url "https://github.com/nwchemgit/nwchem/releases/download/v7.2.0-release/nwchem-7.2.0-release.revision-d0d141fd-src.2023-03-10.tar.bz2"
+  version "7.2.0"
+  sha256 "f756073ab3206571a22ec26cc95dac674fed9c4c959f444b8a97df059ffa3456"
   license "ECL-2.0"
-  revision 3
 
   livecheck do
     url "https://github.com/nwchemgit/nwchem.git"
@@ -30,24 +29,6 @@ class Nwchem < Formula
   depends_on "scalapack"
 
   uses_from_macos "libxcrypt"
-
-  # patches for compatibility with python@3.11
-  # https://github.com/nwchemgit/nwchem/issues/271
-  patch do
-    url "https://github.com/nwchemgit/nwchem/commit/638401361c6f294164a4f820ff867a62ac836fd5.patch?full_index=1"
-    sha256 "20516447b75bde548eb7e40faafcc5d310e8236a7cd3e44f53a753ac1312530e"
-  end
-
-  patch do
-    url "https://github.com/nwchemgit/nwchem/commit/cd0496c6bdd58cf2f1004e32cb39499a14c4c677.patch?full_index=1"
-    sha256 "1ff3fdacdebb0f812f6f14c423053a12f2389b0208b8809f3ab401b066866ffc"
-  end
-
-  # patch for compatibility with ARM
-  patch do
-    url "https://github.com/nwchemgit/nwchem/commit/2a14c04f.patch?full_index=1"
-    sha256 "3a14bb5312861948a468a02a0a079a730e8d9db98d2f2758076f9cd649a6fc04"
-  end
 
   def install
     pkgshare.install "QA"
@@ -75,13 +56,15 @@ class Nwchem < Formula
       ENV["LAPACK_LIB"] = "-L#{Formula["openblas"].opt_lib} -lopenblas"
       ENV["BLAS_SIZE"] = "4"
       ENV["SCALAPACK"] = "-L#{Formula["scalapack"].opt_prefix}/lib -lscalapack"
+      ENV["SCALAPACK_SIZE"] = "4"
       ENV["USE_64TO32"] = "y"
       os = OS.mac? ? "MACX64" : "LINUX64"
-      system "make", "nwchem_config", "NWCHEM_MODULES=all python"
+      system "make", "nwchem_config", "NWCHEM_MODULES=all python gwmol", "USE_MPI=Y"
       system "make", "NWCHEM_TARGET=#{os}", "USE_MPI=Y"
 
       bin.install "../bin/#{os}/nwchem"
       pkgshare.install "basis/libraries"
+      pkgshare.install "basis/libraries.bse"
       pkgshare.install "nwpw/libraryps"
       pkgshare.install Dir["data/*"]
     end
